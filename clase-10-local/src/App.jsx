6731fb6601d799f62861e05bdb599afa23dfa88b
Melisa Marcela Martinez Martinez <melisamartinez2801@gmail.com>
2023-03-07 22:31:29 -0500

    Actividad clase 10: Marzo 7, 2023

---
+++ clase-10-local/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Card from "./Components/Card/Card"
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Carga estudiante: Melisa Martinez</h1>
+      <form></form>
+      <Card />
+    </div>
+  )
+}
+
+export default App
